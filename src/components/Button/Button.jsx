e29187c42e764bad365845848db834f07f96333d
rougenij <rougenij@gmail.com>
2021-12-25 13:37:27 +0200

    Almost done with style

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -4,7 +4,8 @@ import "./button.css";
 const Button = (props) => {
   return (
     <div role={"button"} className="btn" onClick={props.handleClick}>
-      {props.name}
+      {props.name}{" "}
+      <img src={props.iconURL} alt="button-img" className="btn-img" />
     </div>
   );
 };
